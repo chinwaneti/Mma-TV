f11ec0a18472b5b4f2070fc5dc3dc7dffcf36593
monielover <chidinmanwaneti.cn@gmail.com>
2023-07-16 15:54:50 -0700

    feat:add files

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,9 @@
 "use client"
 import Link from 'next/link'
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Navbar() {
-  const [isNightMode, setIsNightMode] = useState(false);
 
-  const toggleTheme = () => {
-    setIsNightMode(prev => !prev);
-  };
 
   return (
     <div>
@@ -15,12 +11,12 @@ export default function Navbar() {
       <nav className={`fixed  w-[95%] px-4 z-50 `}>
         <ul className="flex justify-center item-center space-x-9">
           <Link href="/">
-            <li className=" hover:text-3xl hover:text-red-500 text-xl font-bold mt-10">
+            <li className=" hover:text-3xl text-red-500 text-xl font-bold mt-10">
               HOME
             </li>
           </Link>
           <Link href="/categories">
-            <li className=" hover:text-3xl hover:text-red-500 text-xl font-bold mt-10">
+            <li className=" hover:text-3xl text-red-500 text-xl font-bold mt-10">
               SHOWS
             </li>
           </Link>
